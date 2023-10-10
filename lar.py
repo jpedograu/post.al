687529a3d3157e2c271ae8fab13e09ce48e600d8
jpdograu <ultrasenha@gmail.com>
2023-10-10 11:30:00 -0300

    post.lag

---
+++ lar.py
@@ -0,0 +1,5 @@
+al=float(input('qual a altura da sua parede?'))
+lg=float(input('qual a largura da sua parede'))
+area=al*lg
+tinta=area/2
+print('a area da sua parede é:{}m² e atinta q vc vai gastar é {}L'.format(area,tinta))
